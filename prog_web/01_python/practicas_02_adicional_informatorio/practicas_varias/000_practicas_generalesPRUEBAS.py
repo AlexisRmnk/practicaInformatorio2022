524db6af47d4204d08e0d2ef50fc608b3b67c0ec
Alexis Romaniuk <alexisromaniuk@gmail.com>
2026-01-08 10:26:51 -0300

    20250108 repaso lambda functions

--- prog_web/01_python/practicas_02_adicional_informatorio/practicas_varias/000_practicas_generalesPRUEBAS.py
+++ prog_web/01_python/practicas_02_adicional_informatorio/practicas_varias/000_practicas_generalesPRUEBAS.py
@@ -1,19 +1,8 @@
+from functools import reduce
+depositos = [20, 50, 10]
 
-# ejemplo de practica mio
-def su_suma_es_par(numero1,numero2):
-    if ( (numero1+numero2) % 2 == 1):
-        return False
-    else:
-        return True
+# Sin initializer empieza en 20 + 50...
+# Con initializer (100), empieza calculando 100 + 20...
+total = reduce(lambda saldo, ingreso: saldo + ingreso, depositos, 100)
 
-print('(2 + 1) es par?', su_suma_es_par(2,1))
-print('(3 + 3) es par?', su_suma_es_par(3,3))
-print('(4 + 3) es par?', su_suma_es_par(4,3))
-print('(5 + 1) es par?', su_suma_es_par(5,1))
-
-# ejemplo lambda
-lmbd_suma_par = lambda n1,n2: (n1+n2) % 2 == 1
-print('lmbd (2 + 1) es par?', lmbd_suma_par(2,1))
-print('lmbd (3 + 3) es par?', lmbd_suma_par(3,3))
-print('lmbd (4 + 3) es par?', lmbd_suma_par(4,3))
-print('lmbd (5 + 1) es par?', lmbd_suma_par(5,1))
+print(total) # 180 (100 inicial + 20 + 50 + 10)
